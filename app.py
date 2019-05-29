1c2b2e503ebf7fdd5084df5aee5131dd38c28cf1
Gabe <medina.gabe@gmail.com>
2019-05-29 09:55:46 -0700

    1.1 added a view button
    1 click to view the contents of the database

--- app.py
+++ app.py
@@ -13,10 +13,13 @@ app.secret_key = os.environ['APP_SECRET_KEY']
 def add_item():
     form = ItemForm()
     if form.validate_on_submit():
-        item = Items(name=form.name.data, quantity=form.quantity.data, description=form.description.data, date_added=datetime.datetime.now())
-        db_session.add(item)
-        db_session.commit()
-        return redirect(url_for('success'))
+        if 'send' in request.form:
+            item = Items(name=form.name.data, quantity=form.quantity.data, description=form.description.data, date_added=datetime.datetime.now())
+            db_session.add(item)
+            db_session.commit()
+            return redirect(url_for('success'))
+        elif 'view' in request.form:
+            return redirect(url_for('list'))
     return render_template('index.html', form=form)
 
 @app.route('/success')
@@ -29,6 +32,15 @@ def success():
     strResult = ("\n".join(str(row) for row in results))
     return Response(strResult, mimetype='text/plain')
 
+@app.route('/list')
+def list():
+    results = []
+ 
+    qry = db_session.query(Items)
+    results = qry.all()
+
+    strResult = ("\n".join(str(row) for row in results))
+    return Response(strResult, mimetype='text/plain')
 
 
 if __name__ == '__main__':
